3c745e039dc97e2df0500b8b2ccab59d192319da
stephanie-VM <stephaniecetav@gmail.com>
2020-09-28 20:59:04 -0600

    Js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+function abrir(){
+    document.getElementById('vent').style.display='block';
+    document.getElementById('overlay').style.display='block';
+}
+
+function cerrar(){
+    document.getElementById('vent').style.display='none';
+    document.getElementById('overlay').style.display='none';
+}
